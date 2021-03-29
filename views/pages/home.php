4857c0395f202b33df1563b9e94bfb80312f644e
deempalme <20066249+deempalme@users.noreply.github.com>
2021-03-29 01:30:01 -0700

    home page improved and new images were also added

--- views/pages/home.php
+++ views/pages/home.php
@@ -6,7 +6,7 @@
 <title><?= $controller->page_title(); ?></title>
 <link rel="icon" type="image/png" href="/resources/theme/icon.png" />
 <link href="/resources/css/home.css" rel="stylesheet" />
-<script language="javascript" src="/resources/js/home.js" type="text/javascript"></script>
+<script language="javascript" src="/script/js/home.js" type="text/javascript"></script>
 </head>
 
 <body>
@@ -15,135 +15,169 @@
       <li><a href="#universe">Top</a></li>
       <li><a href="#portfolio">Portfolio</a></li>
       <li><a href="#projects">Projects</a></li>
-      <li><a href="#knowledge">Knowledge</a></li>
       <li><a href="#about_me">About me</a></li>
+      <li><a href="#knowledge">Knowledge</a></li>
       <li><a href="#studies">Studies</a></li>
       <li><a href="#contact_me">Contact me</a></li>
     </ul>
   </nav>
   <main>
     <section id="universe" name="universe">
+      <h1><img src="/resources/theme3.0/ramirez_rodriguez.svg" alt="RAMIREZ RODRIGUEZ"></h1>
+    </section>
+    <section id="portfolio" name="portfolio">
+      <h1><img src="/resources/theme3.0/the_portfolio.svg" alt="The PORTFOLIO"></h1>
+    </section>
+    <section id="projects" name="projects">
+      <h1><img src="/resources/theme3.0/projects.svg" alt="PROJECTS"></h1>
+    </section>
+    <section id="about_me" name="about_me">
+      <h1><img src="/resources/theme3.0/about_me.svg" alt="ABOUT ME"></h1>
     </section>
     <section id="knowledge" name="knowledge">
-      <ul>
-        <li>3D design & construction:</li>
-        <ul>
-          <li>CAD desing</li>
-          <ul>
-            <li>Autocad</li>
-            <li>Solidworks</li>
-            <li>Inventor</li>
-            <li>Blender</li>
-          </ul>
-          <li>CAM programming</li>
-          <ul>
-            <li>MasterCAM</li>
-          </ul>
-          <li>CNC machining</li>
-          <ul>
-            <li>HAAS</li>
-            <li>Centroid</li>
-            <li>Yamaha</li>
-          </ul>
-          <li>Finite element analysis</li>
-          <ul>
-            <li>ANSYS</li>
-            <li>Solidworks</li>
-          </ul>
-          <li>3D printing methods & processes</li>
-        </ul>
-        <li>Automation:</li>
-        <ul>
-          <li>Hydraulics</li>
-          <li>Neumatics</li>
-          <li>PLC design</li>
-          <li>Robotic inclusion</li>
-          <li>Microcontrollers</li>
-          <ul>
-            <li>Microchip</li>
-            <li>Raspberry PI</li>
-            <li>Arduino</li>
-          </ul>
-        </ul>
-        <li>Programming:</li>
-        <ul>
-          <li>C++</li>
-          <li>Python</li>
-          <li>ROS (Robot Operating System)</li>
-          <li>OpenGL</li>
-          <li>Matlab C++</li>
-          <li>Network programming & serial communications</li>
-          <li>SQL</li>
-          <li>Php</li>
-          <li>Typescript</li>
-          <li>Javascript</li>
-          <li>CSS 2</li>
-          <li>XML</li>
-          <li>HTML 5</li>
-          <li>Parallel programming:</li>
-          <ul>
-            <li>GLSL (OpenGL Shading Language)</li>
-            <li>CPU's multi-thread programming</li>
-            <li>CUDA (Nvidia's Compute Unified Device Architecture)</li>
-          </ul>
-        </ul>
-        <li>Electronics:</li>
-        <ul>
-          <li>Digital electronics</li>
-          <li>Power electronics</li>
-          <li>Electronic circuits' design & construction</li>
-          <li>Microcontrollers</li>
-        </ul>
-        <li>Mathematics:</li>
-        <ul>
-          <li>Linear algebra</li>
-          <li>Trigonometry</li>
-          <li>Arithmetics</li>
-          <li>Computed linear equations</li>
-        </ul>
-        <li>Autonomous driving:</li>
-        <ul>
-          <li>Image processing</li>
-          <li>LiDAR's data reading & processing</li>
-          <li>Grid mapping calculation</li>
-          <li>Map's information processing</li>
-          <li>Navigation</li>
-          <li>Trajectory calculation</li>
-          <li>Virtual reality & game engines</li>
-          <li>Real-time rendering</li>
-        </ul>
-      </ul>
+      <h1><img src="/resources/theme3.0/knowledge.svg" alt="KNOWLEDGE"></h1>
+      <article>
+        <ol>
+          <li><b>3D Design & Construction:</b></li>
+          <ol>
+            <li>CAD desing</li>
+            <ol>
+              <li>Autocad</li>
+              <li>Solidworks</li>
+              <li>Inventor</li>
+              <li>Blender</li>
+            </ol>
+            <li>CAM programming</li>
+            <ol>
+              <li>MasterCAM</li>
+            </ol>
+            <li>CNC machining</li>
+            <ol>
+              <li>HAAS</li>
+              <li>Centroid</li>
+              <li>Yamaha</li>
+            </ol>
+            <li>Finite element analysis</li>
+            <ol>
+              <li>ANSYS</li>
+              <li>Solidworks</li>
+            </ol>
+            <li>3D printing methods & processes</li>
+          </ol>
+          <li><b>Automation:</b></li>
+          <ol>
+            <li>Hydraulics</li>
+            <li>Neumatics</li>
+            <li>PLC design</li>
+            <li>Robotic inclusion</li>
+            <li>Microcontrollers</li>
+            <ol>
+              <li>Microchip</li>
+              <li>Raspberry PI</li>
+              <li>Arduino</li>
+            </ol>
+          </ol>
+        </ol>
+        <ol>
+          <li><b>Programming:</b></li>
+          <ol>
+            <li>C++</li>
+            <li>Python</li>
+            <li>ROS (Robot Operating System)</li>
+            <li>OpenGL</li>
+            <li>Matlab C++</li>
+            <li>Network programming & serial communications</li>
+            <li>SQL</li>
+            <li>Php</li>
+            <li>Typescript</li>
+            <li>Javascript</li>
+            <li>CSS 2</li>
+            <li>XML</li>
+            <li>HTML 5</li>
+            <li>Parallel programming:</li>
+            <ol>
+              <li>GLSL (OpenGL Shading Language)</li>
+              <li>CPU's multi-thread programming</li>
+              <li>CUDA (Compute Unified Device Architecture)</li>
+            </ol>
+          </ol>
+          <li><b>Electronics:</b></li>
+          <ol>
+            <li>Digital electronics</li>
+            <li>Power electronics</li>
+            <li>Electronic circuits' design & construction</li>
+            <li>Microcontrollers</li>
+          </ol>
+        </ol>
+        <ol>
+          <li><b>Mathematics:</b></li>
+          <ol>
+            <li>Linear algebra</li>
+            <li>Trigonometry</li>
+            <li>Arithmetics</li>
+            <li>Computed linear equations</li>
+          </ol>
+          <li><b>Autonomous driving:</b></li>
+          <ol>
+            <li>Image processing</li>
+            <li>LiDAR's data reading & processing</li>
+            <li>Grid mapping calculation</li>
+            <li>Map's information processing</li>
+            <li>Navigation</li>
+            <li>Trajectory calculation</li>
+            <li>Virtual reality & game engines</li>
+            <li>Real-time rendering</li>
+          </ol>
+        </ol>
+      </article>
     </section>
     <section id="studies" name="studies">
+      <h1><img src="/resources/theme3.0/studies.svg" alt="STUDIES"></h1>
       <ul>
         <li>
-          <h1><img src="/resources/theme/ith.jpg"></h1>
-          <small>Graduated at</small>
-          <h2>Hermosillo Institute <i>of</i> Technology</h2>
-          <p><b>2006 - 2011</b>, Mechatronics engineer</p>
+          <figure><img src="/resources/theme/ith.jpg"></figure>
+          <small><i>Graduated at</i></small>
+          <h1>Hermosillo Institute <i>of</i> Technology</h1>
+          <p><b>2006 - 2011</b>, B. Eng. In Mechatronic Engineering</p>
         </li>
         <li>
-          <h1><img src="/resources/theme/fh.jpg"></h1>
-          <small>Graduated at</small>
-          <h2>Fachhochschule <i>of</i> Aachen</h2>
-          <p><b>2014 - 2017</b>, Master degree in Mechatronics</p>
+          <figure><img src="/resources/theme3.0/fh.svg"></figure>
+          <small><i>Graduated at</i></small>
+          <h1>Fachhochschule <i>of</i> Aachen</h1>
+          <p><b>2014 - 2017</b>, M. Sc. In Mechatronics</p>
+        </li>
+        <li>
+          <figure><img src="/resources/theme3.0/rwth_logo.svg"></figure>
+          <small><i>Studied at</i></small>
+          <h1>Rheinisch-Westfälische Technische<br><b>Hochschule Aachen</b></h1>
+          <p><b>2017-2020</b>, Ph. D. in Production engineering</p>
         </li>
       </ul>
     </section>
   </main>
   <footer id="contact_me" name="contact_me">
-    <h1>Contact me</h1>
-    <form action="/contact_me" method="post">
-      <label for="name_field">What's your name:</label>
-      <input type="text" id="name_field" name="name_field" value="" placeholder="Name & last name" required>
-      <label for="email_field">What's your email:</label>
-      <input type="email" id="email_field" name="email_field" value="deempalme@gmail.com" required>
-      <label for="phone_field">What's your cellphone:</label>
-      <input type="text" id="phone_field" name="phone_field" value="743574985">
-      <label for="comment_field">What would you like to say to me:</label>
-      <textarea name="comment_field" id="comment_field" cols="30" rows="10" placeholder="Put your comment" required></textarea>
-      <button type="submit" value="gone">Send</button>
-    </form>
+    <h1><img src="/resources/theme3.0/contact_me.svg" alt="CONTACT ME"></h1>
+    <section>
+      <small>Email</small>
+      <p><b>f.j.r.r@ramrod.tech</b></p>
+    </section>
+    <section>
+      <small>Whatsapp</small>
+      <p><b>+52 622 125 3017</b></p>
+    </section>
+    <section>
+      <small>Signal</small>
+      <p><b>+49 575 145 6778</b></p>
+    </section>
+    <ul>
+      <li><a href="#top">Top</a></li>
+      <li><a href="#portfolio">Portfolio</a></li>
+      <li><a href="#projects">Projects</a></li>
+      <li><a href="#about_me">About me</a></li>
+      <li><a href="#knowledge">Knowledge</a></li>
+      <li><a href="#studies">Studies</a></li>
+      <li><a href="#contact_me">Contact me</a></li>
+    </ul>
   </footer>
 </body>
 
